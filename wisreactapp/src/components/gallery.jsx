04b3bfd9c2ab973a34112bbdf872df34550ce8bc
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2023-11-22 18:36:07 -0500

    updated events section to be properly formatted

--- wisreactapp/src/components/gallery.jsx
+++ wisreactapp/src/components/gallery.jsx
@@ -6,22 +6,18 @@ export const Gallery = (props) => {
       <div className='container'>
         <div className='section-title'>
           <h2>Events</h2>
-          <p>
-           Here are some of our past and future events!
-          </p>
+          <p>Here are some of our past and future events!</p>
         </div>
         <div className='row'>
-          <div className='portfolio-items'>
-            {props.data
-              ? props.data.map((d, i) => (
+          {props.data
+            ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
                   <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
                 </div>
               ))
-              : 'Loading...'}
-          </div>
+            : 'Loading...'}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
