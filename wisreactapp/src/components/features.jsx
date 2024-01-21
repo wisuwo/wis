9843a8d98095a9b5af9e560738fa15a3fb15c4e3
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-01-21 15:28:37 -0500

    fixing nav bar and other small things

--- wisreactapp/src/components/features.jsx
+++ wisreactapp/src/components/features.jsx
@@ -1,35 +1,27 @@
 export const Features = (props) => {
   return (
     <div id='schedule' className='text-center'>
-      <div className='container'>
-        <div className='col-md-10 col-md-offset-1 section-title'>
-
-        </div>
-
-
         <div className='section-title'>
           <h2>Calendar</h2>
           <p>
             Stay updated with all of our upcoming events.
           </p>
-
-          <div>
-            <iframe src="https://calendar.google.com/calendar/embed?src=sshb5he2u8ak5t72cjdu7dgefc%40group.calendar.google.com&ctz=America%2FNew_York"
-              width="800"
-              height="500"
-            ></iframe>
-
+          <div className='row justify-content-center'>
+            <div className='col-md-8'>
+              <iframe src="https://calendar.google.com/calendar/embed?src=sshb5he2u8ak5t72cjdu7dgefc%40group.calendar.google.com&ctz=America%2FNew_York"
+                width="100%"
+                height="500"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+                title="calendar"
+                className="mx-auto"
+              >
+                Loading…
+              </iframe>
+            </div>
           </div>
-
-
-
-
         </div>
-
-
-
-      </div>
     </div>
   )
-
 }
