e0ce5c3907ddea98d3d49cc2917ebb81d9f94f1f
Ramandeep <ramandeepsaini1379@gmail.com>
2024-11-16 22:25:22 -0500

    footer feedback link added

--- wisreactapp/src/components/footer.jsx
+++ wisreactapp/src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 /* 
 Here is the Footer component, displayed on each page of the website. It consists of a
-simple text crediting the Women in Science team for creating the website.
+simple text crediting the Women in Science team for creating the website, along with a link for feedback.
 */ 
 
 export const Footer = (props) => {
@@ -11,10 +11,16 @@ export const Footer = (props) => {
         <div id='footer'>
             <div className='container text-center'>
                 <p>
-                    &copy; 2024 Created with Love by Women In Science at Western University.
+                    &copy; 2024 Created with Love by Women In Science at Western University. 
+                    {' '}
+                    Have any feedback? Let us know{' '}
+                    <a href="https://forms.gle/qzJPGLW3aRCDWGNV9" target="_blank" rel="noopener noreferrer">
+                        here
+                    </a>.
                 </p>
             </div>
         </div>
     );
 }
+
 
