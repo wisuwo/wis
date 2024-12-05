89108db8fe1dc0d7e2089dc412935326f54756fd
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-12-05 14:31:45 -0500

    adding comments

--- wisreactapp/src/components/calendar.jsx
+++ wisreactapp/src/components/calendar.jsx
@@ -1,23 +1,27 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 /* 
-Here is the separate Calendar component. It uses the separate Navigation bar. 
-It displays an embedded Google Calendar which shows all the club's past and 
-upcoming events.
-*/ 
+The Calendar component displays an embedded Google Calendar 
+to showcase all past and upcoming events for the club. It uses 
+a `trigger` prop to conditionally render the calendar. 
+A loading indicator is displayed while the iframe is loading.
+*/
 
 function Calendar(props) {
-
+  // State to track whether the iframe is still loading
   const [isLoading, setIsLoading] = useState(true);
 
+  // Scrolls to the top of the page when the component is mounted
   useEffect(() => { 
     window.scrollTo(0, 0);
   }, [] );
 
+  // Handler to stop showing the loading indicator once the iframe has loaded
   function handleLoading() {
     setIsLoading(false);
   }
 
+  // Renders the Calendar component only if the `trigger` prop is true
   return (props.trigger) ? (
     <div id='schedule' className='text-center'>
       <div className='container'>
@@ -29,23 +33,27 @@ function Calendar(props) {
             Stay updated with all of our upcoming events.
           </p>
         </div>
-        <div className=''> 
+
+        <div className=''>
           <div className=''>
+            {/* Show a loading indicator while the calendar is loading */}
             {isLoading && (
               <div className="loading-indicator">
-                <img src="img/icons/favicon.ico.png" className="loading"></img>
+                <img src="img/icons/favicon.ico.png" className="loading" alt="Loading indicator"></img>
               </div>
             )}
-            <iframe src="https://calendar.google.com/calendar/embed?src=7d3cd944488769a5d31072863a482a8334024049eff407b9494887bc4f05e7ba%40group.calendar.google.com&ctz=America%2FToronto"              
+
+            {/* Embed the Google Calendar iframe */}
+            <iframe
+              src="https://calendar.google.com/calendar/embed?src=7d3cd944488769a5d31072863a482a8334024049eff407b9494887bc4f05e7ba%40group.calendar.google.com&ctz=America%2FToronto"
               width="90%"
               height="700px"
               title="calendar"
               className="mx-auto"
-              onLoad={handleLoading}
-              style={
-                {display: (isLoading) ? "none" : "block"
-                }
-              }
+              onLoad={handleLoading} // Stop loading once iframe is loaded
+              style={{
+                display: isLoading ? "none" : "block" // Hide iframe while loading
+              }}
             >
               Loading…
             </iframe>
@@ -53,7 +61,7 @@ function Calendar(props) {
         </div>
       </div>
     </div>
-  ) : "";
+  ) : ""; // Render nothing if `trigger` is false
 }
 
 export default Calendar;
--- wisreactapp/src/components/join.jsx
+++ wisreactapp/src/components/join.jsx
@@ -1,24 +1,36 @@
 import { Link } from "react-router-dom";
 
 /* 
-Here is the Join component displayed on the Home page.
-It consists of an embedded Google Form for users to sign up for the club.
-It also consists of a button that links to the separate Contact Us page.
-*/ 
+The Join component is displayed on the Home page and provides a way for users 
+to sign up for the club via an embedded Google Form. It also includes a button 
+that links to the Contact Us page for further inquiries. 
+
+Key Features:
+- Embedded Google Form: Allows users to fill out the sign-up form directly on the page.
+- External Form Link: Provides a link to open the form in a new tab.
+- Contact Us Button: Navigates to the Contact Us page for additional communication.
+*/
 
 export const Join = (props) => {
   return (
     <div id='join' className='text-center'>
       <div className='section-title'>
         <h2>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfZi63fsrFA4vDmpCkVh68dco-r5WRv-WvMYfFuPoJtKAiwaw/viewform" target="_blank" rel="noreferrer">
+          {/* External link to the Google Form */}
+          <a 
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfZi63fsrFA4vDmpCkVh68dco-r5WRv-WvMYfFuPoJtKAiwaw/viewform" 
+            target="_blank" 
+            rel="noreferrer" // Ensures secure and safe external linking
+          >
             Let's Stay Connected
           </a>
         </h2>
         <p>
           Ready to take a stand against the gender disparities in STEM? Join our club.
         </p>
-        <div >
+
+        {/* Embedded Google Form */}
+        <div>
           <div>
             <iframe
               src="https://docs.google.com/forms/d/e/1FAIpQLSfZi63fsrFA4vDmpCkVh68dco-r5WRv-WvMYfFuPoJtKAiwaw/viewform?embedded=true"
@@ -34,15 +46,14 @@ export const Join = (props) => {
             </iframe>
           </div>
         </div>
-        {/*<div>
-          <p>
-            Don't miss any important info, sign up for our monthly newsletter!
-          </p>
-        </div> */}
       </div>
+
+      {/* Button linking to the Contact Us page */}
       <div className='calendar-link'>
-          <Link to="/contact"><button className="btn btn-custom btn-lg">Contact Us</button></Link>
-          </div>
+        <Link to="/contact">
+          <button className="btn btn-custom btn-lg">Contact Us</button>
+        </Link>
+      </div>
     </div>
   );
 };
