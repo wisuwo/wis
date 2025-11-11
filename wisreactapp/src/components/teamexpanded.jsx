7b3a13e3ac7d226c039b0d78e7b5b53d3921a3c9
Prab0525 <Prabhjot.kaur0526@gmail.com>
2025-11-11 14:32:20 -0500

    Update team member information and bios

--- wisreactapp/src/components/teamexpanded.jsx
+++ wisreactapp/src/components/teamexpanded.jsx
@@ -6,96 +6,115 @@ import { Footer } from "./footer";
 
 // includes team member information
 const teamMembers = [
+// Co-preidents
   {
-    name: 'Bobo Soong',
-    photo: 'img/team/Bobo 3.JPG', 
-    bio: 'I wanted to join a supportive community that offers opportunities for networking, advocacy, and personal and professional growth. Favourite Food: Sushi, Least Favourite Food: Rhubarb',
+    name: 'Julie Le',
+    photo: 'img/team/Julie.JPG', 
+    bio: 'I first joined WiS for the incredible mentorship opportunities, but I’ve stayed because of the amazing people I’ve met along the way.',
   },
   
   {
-    name: 'Cristina White',
-    photo: 'img/team/Cristina.jpg',
-    bio: 'To be part of and contribute to an incredible organization that provides community and support for students. Favourite Food: My grandma’s lasagna, Least Favourite Food: Pickles'
+    name: 'Bobo Soong',
+    photo: 'img/team/Bobo.JPG',
+    bio: 'I joined WiS for the supportive community and amazing members!'
   },
 
   {
-    name: 'Emaan Rana',
-    photo: 'img/team/Emaan .JPG',
-    bio: 'To join a team of women who support one another while giving back to other students who needed the same guidance I did in my first year. Favourite Food: Noodles, Least Favourite Food: Green beans '
+    name: 'Bahar M',
+    photo: 'img/team/Bahar.jpg',
+    bio: 'I joined WiS to meet people with similar interests, and have stayed because of the amazing friendships I’ve made over the years!
+'
+  },
+
+//VP comms
+  {
+    name: 'Shaen Goodall',
+    photo: 'img/team/Shaen.JPG',
+    bio: "I joined WiS to help create a space where like-minded women in science could feel comfortable, make friends, and help each other succeed!"
   },
 
   {
-    name: 'Sophia Yuan',
-    photo: 'img/team/Copy of Sophia 3.JPG',
-    bio: "To join a community of like minded women in science and support and uplift each other. Favourite Food: Sushi, Least Favourite Food: Black Licorice"
+    name: 'Emma Turgeon',
+    photo: 'img/team/Emma.jpg',
+    bio: 'I joined WiS to support the vibrant community of women in the science field and to help foster meaningful connections that allow us to grow together.'
   },
 
-  {
-    name: 'Karolin Volodko',
-    photo: 'img/team/Karolin 2.JPG',
-    bio: 'To provide an open, inclusive, and empowering community for the Western community. Favourite Food: Sushi, Least Favourite Food: Raw onion'
-  },
-
-  {
-    name: 'Harleen Multani',
-    photo: 'img/team/Harleen 1.JPG',
-    bio: 'To help create a supportive community that empowers women in STEM. Favourite Food: Greek food, Least Favourite Food: Raisins'
-  },
-
+  // VP Externals
   {
     name: 'Kate La',
     photo: 'img/team/Kate.JPG',
-    bio: 'I joined WiS because of the incredibly supportive community I found here and to be able to help empower and meet other women in the fields of science! Favourite Food: Pho, Least Favourite Food: Arugula'
+    bio: 'I joined WiS to meet and connect with other amazing like-minded women in science!'
   },
 
   {
-    name: 'Diana Abdolmaleki',
-    photo: 'img/team/Diana.jpg',
-    bio: 'To help dismantle stereotypes about women and femininity and encourage others to challenge these outdated perceptions. Favourite Food: Kabobs, Least Favourite Food: Mayo'
+    name: 'Melinda Gao',
+    photo: 'img/team/Melinda.JPG',
+    bio: 'I joined WiS to help create a welcoming environment where everyone can feel supported!'
+  },
+
+  // VP tech
+  {
+    name: 'Ramandeep',
+    photo: 'img/team/Ramandeep.JPG',
+    bio: 'I joined WiS to improve my technical skills on the technology team and to help build a community of women in science!'
+  },
+
+  //VP internals
+  {
+    name: 'Claudia Menezes',
+    photo: 'img/team/Claudia.JPG',
+    bio: "I joined WiS to connect with amazing women in science and build a welcoming community filled with exciting events!"
   },
 
   {
     name: 'Trinity Yan',
-    photo: 'img/team/Trinity 4.JPG',
-    bio: "I joined WiS to meet new people and be a part of the supportive, welcoming, and encouraging community! Favourite Food: Tofu Stew, Least Favourite Food: Cherry tomatoes"
+    photo: 'img/team/Trinity.JPG',
+    bio: 'I joined WiS to support and grow alongside like-minded women, and to help build a welcoming community for everyone to join!'
+  },
+
+  // VP mentorship
+  {
+    name: 'Jessica Kay',
+    photo: 'img/team/Jessica.PNG',
+    bio: 'I joined  WiS to gain the mentorship I needed to grow, and I stay involved because I’m passionate about giving back, offering the same support to others that helped me along my journey.'
   },
 
   {
-    name: 'Brielle Hall',
-    photo: 'img/team/Brielle 1.jpg',
-    bio: 'I joined WiS to make an impact on girls studying science at Western and provide meaningful and fun events. Favourite Food: Cheese, Least Favourite Food: Steak'
+    name: 'Sarina Kosari',
+    photo: 'img/team/Sarina.jpeg',
+    bio: "I joined WiS as a mentor and fell in love with the empowerment the club brings to girls who want to pursue science, and now as Vice-President I am committed to expanding that impact on a larger scale!"
+  },
+// VP advocacy
+  {
+    name: 'Areti Koroneos',
+    photo: 'img/team/Areti.jpeg',
+    bio: "I joined WiS to connect and grow with a supportive community of women who share my goals, passions, and interests."
+  },
+// VP Outreach
+  {
+    name: "Chloe Ng",
+    photo: 'img/team/Chloe.jpeg',
+    bio: "I joined WiS because of its friendly and supportive environment. I love hearing the inspiring and diverse perspectives of people in the scientific field."
   },
 
   {
-    name: 'Julie Le',
-    photo: 'img/team/Julie 3.JPG',
-    bio: 'I joined Women in Science because of the incredible friendships, community, and support it provided me, and now I want to give back by fostering that same experience for others. Favourite Food: Spaghetti Carbonara, Least Favourite Food: Tomatoes'
-  },
-
+    name: "Rory Paraskevopoulos",
+    photo: 'img/team/Rory.jpeg',
+    bio: "I joined WiS to be part of a welcoming, empowering community for women in science, and to have inspiring role models to look up too!"
+  }
+// VP Finance 
   {
-    name: 'Bahar Mohammadi',
-    photo: 'img/team/Bahar 1.jpg',
-    bio: "I joined WiS to not only connect with others who share my passion for Science but also to build friendships with people who inspire and support one another. Favourite Food: Sushi, Least Favourite Food: Oranges"
-  },
-
-  {
-    name: 'Lily Zhu',
-    photo: 'img/team/Lily.jpg',
-    bio: "I joined WiS to motivate myself with my goals and inspire others! Favourite Food: KBBQ, Least Favourite Food: Ramen"
-  },
-
-  {
-    name: "Melinda Gao",
-    photo: 'img/team/Melinda 2.JPG',
-    bio: "I joined WIS to help create a supportive community where women can connect and empower each other. Favourite Food: Tacos, Least Favourite Food: Asparagus"
-  },
-
-  {
-    name: "Christina Paraskevopoulos",
-    photo: 'img/team/Christina 2.JPG',
-    bio: "I joined WIS to connect with like-minded individuals and create a welcoming community for women in STEM. Favourite Food: Chicken Fingers, Least Favourite Food: Mustard"
+    name: "Harleen Multani",
+    photo: 'img/team/Harleen.JPG',
+    bio: "I joined WiS because I’m passionate about creating opportunities for women in science to share experiences, resources, and inspiration"
   }
 
+// VP Sponsership
+{
+    name: "Brielle Hall",
+    photo: 'img/team/Bri.JPG',
+    bio: "I joined WiS to give girls at western a community where they can feel welcomed and supported. Bringing the events to WiS members gives me so much joy!"
+  }
 ];
 
 // Ensures the user starts at the top of the page
@@ -141,5 +160,6 @@ function MeetTheTeam() {
     </>
   );
 }
+
 
 export default MeetTheTeam;
