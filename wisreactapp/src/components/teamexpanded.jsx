967e311adc7662d9b392e07f2b996695b0791c92
Prab0525 <Prabhjot.kaur0526@gmail.com>
2025-11-11 14:50:10 -0500

    Include favorite movies for team members
    Added favorite movie information for each team member.

--- wisreactapp/src/components/teamexpanded.jsx
+++ wisreactapp/src/components/teamexpanded.jsx
@@ -11,108 +11,125 @@ const teamMembers = [
     name: 'Julie Le',
     photo: 'img/team/Julie.JPG', 
     bio: 'I first joined WiS for the incredible mentorship opportunities, but I’ve stayed because of the amazing people I’ve met along the way.',
+    favMovie: 'Favourite movie: Sinners'
   },
   
   {
     name: 'Bobo Soong',
     photo: 'img/team/Bobo.JPG',
-    bio: 'I joined WiS for the supportive community and amazing members!'
+    bio: 'I joined WiS for the supportive community and amazing members!',
+    favMovie: 'Favourite movie: Glass Onion'
   },
 
   {
     name: 'Bahar M',
     photo: 'img/team/Bahar.jpg',
-    bio: 'I joined WiS to meet people with similar interests, and have stayed because of the amazing friendships I’ve made over the years!'
+    bio: 'I joined WiS to meet people with similar interests, and have stayed because of the amazing friendships I’ve made over the years!',
+    favMovie: 'Favourite movie: The Godfather'
   },
 
 //VP comms
   {
     name: 'Shaen Goodall',
     photo: 'img/team/Shaen.JPG',
-    bio: "I joined WiS to help create a space where like-minded women in science could feel comfortable, make friends, and help each other succeed!"
+    bio: "I joined WiS to help create a space where like-minded women in science could feel comfortable, make friends, and help each other succeed!",
+    favMovie: 'Favourite movie: The Truman Show'
   },
 
   {
     name: 'Emma Turgeon',
     photo: 'img/team/Emma.jpg',
-    bio: 'I joined WiS to support the vibrant community of women in the science field and to help foster meaningful connections that allow us to grow together.'
+    bio: 'I joined WiS to support the vibrant community of women in the science field and to help foster meaningful connections that allow us to grow together.',
+    favMovie: 'Favourite movie: Christopher Robin'
   },
 
   // VP Externals
   {
     name: 'Kate La',
     photo: 'img/team/Kate.JPG',
-    bio: 'I joined WiS to meet and connect with other amazing like-minded women in science!'
+    bio: 'I joined WiS to meet and connect with other amazing like-minded women in science!',
+    favMovie: 'Favourite movie: Divergent'
   },
 
   {
     name: 'Melinda Gao',
     photo: 'img/team/Melinda.JPG',
-    bio: 'I joined WiS to help create a welcoming environment where everyone can feel supported!'
+    bio: 'I joined WiS to help create a welcoming environment where everyone can feel supported!',
+    favMovie: 'Favourite movie: Despicable Me'
   },
 
   // VP tech
   {
     name: 'Ramandeep',
     photo: 'img/team/Ramandeep.JPG',
-    bio: 'I joined WiS to improve my technical skills on the technology team and to help build a community of women in science!'
+    bio: 'I joined WiS to improve my technical skills on the technology team and to help build a community of women in science!',
+    favMovie: 'Favourite movie: The Glassworker'
   },
 
   //VP internals
   {
     name: 'Claudia Menezes',
     photo: 'img/team/Claudia.JPG',
-    bio: "I joined WiS to connect with amazing women in science and build a welcoming community filled with exciting events!"
+    bio: "I joined WiS to connect with amazing women in science and build a welcoming community filled with exciting events!",
+    favMovie: 'Favourite movie: Tangled'
   },
 
   {
     name: 'Trinity Yan',
     photo: 'img/team/Trinity.JPG',
-    bio: 'I joined WiS to support and grow alongside like-minded women, and to help build a welcoming community for everyone to join!'
+    bio: 'I joined WiS to support and grow alongside like-minded women, and to help build a welcoming community for everyone to join!',
+    favMovie: 'Favourite movie: Interstellar'
   },
 
   // VP mentorship
   {
     name: 'Jessica Kay',
     photo: 'img/team/Jessica.PNG',
-    bio: 'I joined  WiS to gain the mentorship I needed to grow, and I stay involved because I’m passionate about giving back, offering the same support to others that helped me along my journey.'
+    bio: 'I joined  WiS to gain the mentorship I needed to grow, and I stay involved because I’m passionate about giving back, offering the same support to others that helped me along my journey.',
+    favMovie: 'Favourite movie: Nerve'
   },
 
   {
     name: 'Sarina Kosari',
     photo: 'img/team/Sarina.jpeg',
-    bio: "I joined WiS as a mentor and fell in love with the empowerment the club brings to girls who want to pursue science, and now as Vice-President I am committed to expanding that impact on a larger scale!"
+    bio: "I joined WiS as a mentor and fell in love with the empowerment the club brings to girls who want to pursue science, and now as Vice-President I am committed to expanding that impact on a larger scale!",
+    favMovie: 'Favourite movie: The Notebook'
   },
 // VP advocacy
   {
     name: 'Areti Koroneos',
     photo: 'img/team/Areti.jpeg',
-    bio: "I joined WiS to connect and grow with a supportive community of women who share my goals, passions, and interests."
+    bio: "I joined WiS to connect and grow with a supportive community of women who share my goals, passions, and interests.",
+    favMovie: 'Favourite movie: The Substance'
   },
 // VP Outreach
   {
     name: "Chloe Ng",
     photo: 'img/team/Chloe.jpeg',
-    bio: "I joined WiS because of its friendly and supportive environment. I love hearing the inspiring and diverse perspectives of people in the scientific field."
+    bio: "I joined WiS because of its friendly and supportive environment. I love hearing the inspiring and diverse perspectives of people in the scientific field.",
+    favMovie: 'Favourite movie: Little Women'
   },
 
   {
     name: "Rory Paraskevopoulos",
     photo: 'img/team/Rory.jpeg',
-    bio: "I joined WiS to be part of a welcoming, empowering community for women in science, and to have inspiring role models to look up too!"
+    bio: "I joined WiS to be part of a welcoming, empowering community for women in science, and to have inspiring role models to look up too!",
+    favMovie: 'Favourite movie: Cheaper by the Dozen'
   },
 // VP Finance 
   {
     name: "Harleen Multani",
     photo: 'img/team/Harleen.JPG',
-    bio: "I joined WiS because I’m passionate about creating opportunities for women in science to share experiences, resources, and inspiration"
+    bio: "I joined WiS because I’m passionate about creating opportunities for women in science to share experiences, resources, and inspiration",
+    favMovie: 'Favourite movie: The Hitman’s Bodyguard'
   },
 
 // VP Sponsership
 {
     name: "Brielle Hall",
     photo: 'img/team/Bri.JPG',
-    bio: "I joined WiS to give girls at western a community where they can feel welcomed and supported. Bringing the events to WiS members gives me so much joy!"
+    bio: "I joined WiS to give girls at western a community where they can feel welcomed and supported. Bringing the events to WiS members gives me so much joy!",
+    favMovie: 'Favourite movie: Enchanted'
   },
 ];
 
@@ -147,6 +164,11 @@ function MeetTheTeam() {
             <div className="member-infos">
             <h2 className="team-member-name">{member.name}</h2>
               <p>{member.bio}</p>
+              {member.favMovie && ( 
+                <p className="fav-movie">
+                  <strong>Favorite Movie:</strong> {member.favMovie}
+                </p>
+               )}
             </div>
           </div>
         ))}
@@ -162,4 +184,5 @@ function MeetTheTeam() {
 
 
 export default MeetTheTeam;
+
 
